dbc1ee704de3324916412911b63c0a1fdfcddc3e
Jochen Kursawe <jochen.kursawe@gmail.com>
2025-07-08 16:39:34 +0100

    adding cellpose training code

--- scripts/cellpose_analysis.py
+++ scripts/cellpose_analysis.py
@@ -1,8 +1,10 @@
 import cellpose.models
 import cellpose.io
+import cellpose.train
 import os
 import imageio
 import time
+from matplotlib import pyplot as plt
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -108,8 +110,133 @@ def make_cellpose_tracking_movie():
     imageio.mimsave(output_movie_path, rgb_frames, fps=5)
     print(f"Saved tracking movie to {output_movie_path}")
     print(f"Time to create movie: {time.time() - t3:.2f} seconds")
+    
+def train_cellpose_sam():
+    """
+    Train a Cellpose model using Rebeeca's data
+    """
+    t0 = time.time()
+    print("Training Cellpose model")
+    
+    model = cellpose.models.CellposeModel(gpu=True)
 
+    model_name = "abdomen_model"
+
+    # default training params
+    n_epochs = 100
+    # n_epochs = 2
+    learning_rate = 1e-5
+    weight_decay = 0.1
+    batch_size = 1
+    
+    # training data:
+    tifffolder = os.path.join(script_dir,'..','data','tiffstack')
+    tiff_files = sorted([f for f in os.listdir(tifffolder) if f.lower().endswith('.tif') or f.lower().endswith('.tiff')])
+    # print(tiff_files)
+    images = [cellpose.io.imread(os.path.join(tifffolder, f)) for f in tiff_files]
+    # images = cellpose.io.imread(avi_path)
+    # reader = imageio.get_reader(avi_path)
+    # images = [cellpose.io.imread((np.asarray(frame))) for frame in reader]
+    images = images[239:1783:4]
+    print(f"Loaded {len(images)} frames from {tifffolder}")
+    
+    ground_truth_mask_dir = os.path.join(script_dir, '..', 'data', 'masks')
+    gt_mask_files = sorted([f for f in os.listdir(ground_truth_mask_dir) if f.endswith('.png')])
+    ground_truth_masks = [cellpose.io.imread(os.path.join(ground_truth_mask_dir, f)) for f in gt_mask_files]
+    # ground_truth_masks = ground_truth_masks[:len(cellpose_masks)]  # match number of frames
+
+    training_data = images[:160] + images[320:]  # select frames for training
+    training_labels = ground_truth_masks[:160] + ground_truth_masks[320:]  # select corresponding masks for training
+    test_data = images[160:240]  # select frames for testing
+    test_labels = ground_truth_masks[160:240]  # select corresponding masks for testing
+    
+    # training_data = training_data[:1]
+    # training_labels = training_labels[:1]
+    # test_data = test_data[:1]
+    # test_labels = test_labels[:1]
+
+    new_model_path, train_losses, test_losses = cellpose.train.train_seg(model.net,
+                                                            train_data=training_data,
+                                                            train_labels=training_labels,
+                                                            test_data=test_data,
+                                                            test_labels=test_labels,
+                                                            batch_size=batch_size,
+                                                            n_epochs=n_epochs,
+                                                            learning_rate=learning_rate,
+                                                            weight_decay=weight_decay,
+                                                            nimg_per_epoch=len(training_data), # can change this
+                                                            model_name=model_name)
+    print(f"train losses are {train_losses}")
+    print(f"test losses are {test_losses}")
+    
+    print(f"Training completed. Model saved to {new_model_path}")
+    
+    plt.figure(figsize=(4.5,2.5))
+    plt.plot(train_losses, label='Train Loss')
+    plt.plot(test_losses, label='Test Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Training and Test Losses')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(os.path.join(script_dir, '..', 'output', f'{model_name}_losses.pdf'))
+
+def quantify_trained_cellpose_performance():
+    """
+    Quantify the performance of the trained Cellpose model.
+    """
+    t0 = time.time()
+    print("Quantifying performance of trained Cellpose model...")
+    
+    # Load the trained model
+    new_model_path = os.path.join(script_dir, 'models', 'abdomen_model')
+    model = cellpose.models.CellposeModel(gpu=True,
+                                          pretrained_model = new_model_path)
+    
+    # Load test data
+    tifffolder = os.path.join(script_dir,'..','data','tiffstack')
+    tiff_files = sorted([f for f in os.listdir(tifffolder) if f.lower().endswith('.tif') or f.lower().endswith('.tiff')])
+    images = [cellpose.io.imread(os.path.join(tifffolder, f)) for f in tiff_files]
+    images = images[239:1783:4]
+    print(f"Loaded {len(images)} frames from {tifffolder}")
+    validation_images = images[260:320]  # match number of frames
+    # validation_images = validation_images[:1]  # for testing, use only one frame
+    
+    # Load ground truth masks
+    ground_truth_mask_dir = os.path.join(script_dir, '..', 'data', 'masks')
+    gt_mask_files = sorted([f for f in os.listdir(ground_truth_mask_dir) if f.endswith('.png')])
+    ground_truth_masks = [cellpose.io.imread(os.path.join(ground_truth_mask_dir, f)) for f in gt_mask_files]
+    ground_truth_masks = ground_truth_masks[260:320]  # match number of frames
+    
+    # Evaluate the model on the test data
+    cellpose_masks = []
+    for image in validation_images:
+        mask, _, _ = model.eval(image)
+        cellpose_masks.append(mask)
+    
+    # Quantification
+    percent_found, global_iot = quantify_segmentation(cellpose_masks, ground_truth_masks)
+    
+    print(f"Percentage of ground truth masks found: {(100*percent_found):.2f}%")
+    print(f"Global IoT score: {global_iot:.4f}")
+    mp4_path = os.path.join(script_dir, '..', 'output', 'segmentation_comparison_trained_cellpose.mp4')
+    visualize_segmentation(cellpose_masks, ground_truth_masks,mp4_path)
+ 
+    print("Tracking cells over time...")
+    tracked_masks = track_cells_over_time(cellpose_masks)
+    
+    print("Converting tracked masks to RGB frames...")
+    rgb_frames = tracked_masks_to_rgb(tracked_masks)
+    
+    print("Creating movie from tracked masks...")
+    # Create a movie from the masks
+    output_movie_path = os.path.join(script_dir, '..', 'output', 'cellpose_trained_tracking.mp4')
+    imageio.mimsave(output_movie_path, rgb_frames, fps=5)
+    print(f"Saved tracking movie to {output_movie_path}")
+ 
 if __name__ == "__main__":
-    create_cellpose_masks()
-    quantify_cellpose_performance()
-    make_cellpose_tracking_movie()
+    # create_cellpose_masks()
+    # quantify_cellpose_performance()
+    # make_cellpose_tracking_movie()
+    train_cellpose_sam()
+    quantify_trained_cellpose_performance()
